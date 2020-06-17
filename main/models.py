119c69b02bf80415bcc9bc8e5d3950e9d6ce86b1
Conor O Reilly <x17101701@student.ncirl.ie>
2020-06-17 13:17:47 +0100

    added all necessary files, set up on Django and running successfully

---
+++ main/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here. DATABASE
+
+
+class Main(models.Model):
+
+    name = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
