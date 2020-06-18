170c957ef19cbfb016fcaa55aa38816aabd8e42a
Conor O Reilly <x17101701@student.ncirl.ie>
2020-06-18 11:38:32 +0100

    added settings

--- main/views.py
+++ main/views.py
@@ -6,11 +6,14 @@ from .models import Main
 
 def home(request):
 
+    site = Main.objects.get(pk=2)
 
-    return render(request, 'front/home.html')
+    return render(request, 'front/home.html', {'site':site})
 
 
 def about(request):
     
+    site = Main.objects.get(pk=2)
 
-    return render(request, 'front/about.html')
+
+    return render(request, 'front/about.html', {'site':site})
