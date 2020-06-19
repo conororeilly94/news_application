19081ba8fb367e1440dd8a977094062d37185e80
Conor O Reilly <x17101701@student.ncirl.ie>
2020-06-19 12:35:53 +0100

    add news page added

--- news/views.py
+++ news/views.py
@@ -4,7 +4,6 @@ from main.models import Main
 
 # Create your views here. ACTIONS
 
-
 def news_detail(request, word):
     
     site = Main.objects.get(pk=2)
@@ -17,3 +16,24 @@ def news_list(request):
     news = News.objects.all()
 
     return render(request, 'back/news_list.html', {'news':news})
+
+def news_add(request):
+    
+    if request.method == 'POST':
+
+        newstitle = request.POST.get('newstitle')
+        newscat = request.POST.get('newscat')
+        newstxtshort = request.POST.get('newstxtshort')
+        newstxt = request.POST.get('newstxt')
+
+
+        if newstitle == "" or newstxtshort == "" or newstxt == "" or newscat == "":
+            error = "All Fields are Required"
+            return render(request, 'back/error.html', {'error': error})
+
+
+        b = News(name=newstitle, short_txt=newstxtshort, body=newstxt, date="2020", pic="-", writer="-", catname=newscat, catid=0, views=0)
+        b.save()
+        return redirect('news_list')
+
+    return render(request, 'back/news_add.html')
