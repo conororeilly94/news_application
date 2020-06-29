4e765e6e6192e7e72b4d68b614475c2758451dda
Conor O Reilly <x17101701@student.ncirl.ie>
2020-06-29 13:42:19 +0100

    added pagination to news list

--- news/views.py
+++ news/views.py
@@ -8,6 +8,7 @@ from cat.models import Cat
 from trending.models import Trending
 import random
 from comment.models import Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here. ACTIONS
@@ -88,7 +89,19 @@ def news_list(request):
     if perm == 0:        
         news = News.objects.filter(writer=request.user)
     elif perm == 1:
-        news = News.objects.all()
+        newss = News.objects.all()
+        paginator = Paginator(newss,3)
+        page = request.GET.get('page')
+
+        try:
+            news = paginator.page(page)
+
+        except EmptyPage:
+            news = paginator.page(paginator.num_page)
+
+        except PageNotAnInteger:
+            news = paginator.page(1)
+
 
     return render(request, 'back/news_list.html', {'news':news})
 
