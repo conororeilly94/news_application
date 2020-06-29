0a9f6f182b115d89425f67be7a47f46cc3627233
Conor O Reilly <x17101701@student.ncirl.ie>
2020-06-29 16:57:41 +0100

    added import/export CSV functionality

--- cat/views.py
+++ cat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cat
-
+import csv
+from django.http import HttpResponse
 
 
 def cat_list(request):
@@ -41,3 +42,50 @@ def cat_add(request):
         return redirect('cat_list')
     
     return render(request, 'back/cat_add.html')
+
+
+def export_cat_csv(request):
+
+    response = HttpResponse(content_type='txt/csv')
+    response['Content-Disposition'] = 'attachment; filename="cat.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Title', 'Counter'])
+    for i in Cat.objects.all():
+        writer.writerow([i.name,i.count])
+
+    return response
+
+
+def import_cat_csv(request):
+
+    if request.method == 'POST':
+
+        csv_file = request.FILES['csv_file']
+
+        if not csv_file.name.endswith('.csv'):
+            error = "Please Import a CSV File"
+            return render(request, 'back/error.html', {'error': error})
+
+        if csv_file.multiple_chunks():
+            error = "File is Too Large"
+            return render(request, 'back/error.html', {'error': error})
+
+        file_data = csv_file.read().decode("utf-8")
+
+        lines = file_data.split("\n")
+
+        for line in lines: 
+            
+            fields = line.split(",")
+
+            try:
+
+                if len(Cat.objects.filter(name=fields[0])) == 0 and fields[0] != "Title" and fields[0] != "":
+                    b = Cat(name=fields[0])
+                    b.save()
+
+            except:
+                print('Finish')
+
+    return redirect('cat_list')
